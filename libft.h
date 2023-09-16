9db8c82ea60279a302f6c434e67f346a9676a95c
sergey <sergey@aleshin.pro>
2023-09-16 08:55:12 +0200

    toupper, tolower

--- libft.h
+++ libft.h
@@ -30,5 +30,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
