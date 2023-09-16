a361ddccbb6d5be9b3e25afb0776fe03250fd1a4
sergey <sergey@aleshin.pro>
2023-09-16 08:39:44 +0200

    strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saleshin <saleshin@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/14 18:00:28 by saleshin          #+#    #+#             */
+/*   Updated: 2023/08/14 18:34:19 by saleshin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+	size_t	j;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dstsize == 0 || dstsize <= dst_len)
+		return (src_len + dstsize);
+	i = dst_len;
+	j = 0;
+	while (src[j] != '\0' && i < dstsize - 1)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (dst_len + src_len);
+}
