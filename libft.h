71029fcfc4fdad3d9b663fb90ea5cd87d2eaee2b
sergey <sergey@aleshin.pro>
2023-09-24 23:01:19 +0200

    lstnew, lstadd_front, lstsize, lstlast

--- libft.h
+++ libft.h
@@ -18,6 +18,12 @@
 # include <stdio.h>
 # include <unistd.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 int		ft_atoi(const char *str);
 size_t	ft_strlen(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -52,5 +58,9 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
