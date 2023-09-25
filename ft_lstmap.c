2a98322bff0ce987a55899ac59c9fd1d4df7ce03
Sergey Aleshin <saleshin@cbr12s1.42barcelona.com>
2023-09-25 16:07:57 +0200

    lstmap. Everything is OK
    git push

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -15,58 +15,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_node;
-	t_list	*prev;
 
 	new_list = NULL;
-	next = NULL;
-	if (lst != NULL)
-	{
-		
-	}
 	while (lst != NULL)
 	{
-		f(lst->content);
-		new_node = ft_lstnew(lst->content);
+		new_node = ft_lstnew(f(lst->content));
 		if (new_node == NULL)
 		{
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		prev->next = new_node;
-		if (new_list == NULL)
-			new_list = new_node;
-		else
-		{
-			current = new_list;
-			while (current->next != NULL)
-				current = current->next;
-			current->next = new_node;
-		}
+		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
 	}
 	return (new_list);
 }
-/*
-	t_list	*new_list;
-	t_list	*new_node;
-
-	if (lst == NULL)
-		return (NULL);
-	f(lst->content);
-	new_list = ft_lstnew(lst->content);
-	if (new_list == 0)
-		return (NULL);
-	lst = lst->next;
-	while (lst != NULL)
-	{
-		if (f != NULL)
-			f(lst->content);
-		new_node = ft_lstnew(lst->content);
-		if (new_list == 0)
-			return (NULL);
-		new_list->next = lst->next
-		lst = lst->next;
-	}
-	return (new_list);
-}
-*/
